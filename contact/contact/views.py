de556f83e0eb4e05da8ac05fe2a0b345f0e74fd8
Govind Bhusal <np03s200007@heraldcollege.edu.np>
2021-04-14 12:56:23 +0545

    Problem in Contact Need help

--- contact/contact/views.py
+++ contact/contact/views.py
@@ -1,16 +1,16 @@
-from django.shorcuts import render
+from django.shortcuts import render
 from contact.models import ContactUs
-from django.contrib import messsages
+from django.contrib import messages 
 
 def Insertrecord(request):
     if request.method=='POST':
         if request.POST.get('Name') and request.POST.get('Email') and request.POST.get('Messages'):
-            client=ContactUs
+            client=ContactUs()
             client.Name=request.POST.get('Name')
             client.Email=request.POST.get('Email')
             client.Messages=request.POST.get('Messages')
             client.save()
-            messsages.success(request, 'Thankyou for your choosing us!')
-            return render(request, 'contact us.html')
+            messsages.success(request, 'Thankyou for choosing us!')
+            return render(request,'contact_us.html')
         else:
-            return render(request, 'contact us.html')
+            return render(request,'contact_us.html')
