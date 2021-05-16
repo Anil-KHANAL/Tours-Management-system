1ba33ecb7bd00750907d9266cea2f71489756cfc
Pabin Shrestha <np03s200071@heraldcollege.edu.np>
2021-05-16 16:02:00 +0545

    fixing the error occured in the search functionality and solving it

--- search django/search django/story/urls.py
+++ search django/search django/story/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name='story'
 
 urlpatterns = [
+  
      path('', views.story_list, name='story_list'),
      path('<slug:category_slug>', views.story_list, name='story_by_category'),
      path('<int:id>/', views.story_detail, name='story_detail'),
-     path('search/',views.search,name='search'),
+     path('search/',views.search, name='search'),
 
-   # path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 
 ]
